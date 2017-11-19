88568f0a397c713cc6a3fc337068993824224883
g1eny0ung <g1enyy0ung@gmail.com>
2017-11-19 10:28:57 +0800

    Reenable haskell layer

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -57,6 +57,9 @@ values."
               clojure-enable-fancify-symbols t)
      javascript
      html
+     (haskell :variables
+              haskell-completion-backend 'intero
+              haskell-enable-hindent-style "johan-tibell")
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
